ff17ef3d993c2c07c345a0831fb1cf4f708b8c0f
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-03 10:12:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// task 1
+// Q 1 : write a programe that checks whether the given number is positive or negitive .
+const number = 5;
+if (number > 0) {
+    console.log("number is positive ");
+}
+else if (number < 0) {
+    console.log("number is negitive");
+}
+else {
+    console.log("the number is zero");
+}
---
+++ main.ts
@@ -0,0 +1,16 @@
+// task 1
+// Q 1 : write a programe that checks whether the given number is positive or negitive .
+
+const number:number = 5;
+
+if(number > 0){
+
+    console.log("number is positive ");
+}else if (number < 0){
+    console.log("number is negitive");
+    
+}else {
+    console.log("the number is zero");
+    
+}
+
